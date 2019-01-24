6666fe26a39166fbf578c98a56381b33a671ae16
John Klingler <jklingler@protonmail.com>
2019-01-24 00:57:06 -0500

    adds background styles to homepage

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("umm");
+function runOnScroll() {
+	let scrollPos = (document.documentElement.scrollTop > 0) ? document.documentElement.scrollTop : document.body.scrollTop;
+	document.body.className = "gogrey";
+	if (scrollPos > 200) {
+    	document.body.className = "myelomastars";
+	}
+	if (scrollPos > 800) {
+    	document.body.className = "terrier";
+	}
+
+} 
+window.addEventListener("scroll", runOnScroll)
