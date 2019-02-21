317a421005d878fbcadb25ec3c42a36586e7a95f
John Klingler <jklingler@protonmail.com>
2019-02-20 23:17:28 -0500

    adds uma, partners and au work pages

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 console.log("umm");
 function runOnScroll() {
 	let scrollPos = (document.documentElement.scrollTop > 0) ? document.documentElement.scrollTop : document.body.scrollTop;
-	document.body.className = "gogrey";
+	document.body.className = "bg-gogrey";
 	if (scrollPos > 200) {
-    	document.body.className = "myelomastars";
+    	document.body.className = "bg-myelomastars";
 	}
 	if (scrollPos > 800) {
-    	document.body.className = "aizle";
+    	document.body.className = "bg-aizle";
 	}
 	if (scrollPos > 1500) {
-    	document.body.className = "terrier";
+    	document.body.className = "bg-terrier";
 	}
 } 
 window.addEventListener("scroll", runOnScroll)
