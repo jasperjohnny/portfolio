987ad2602c70286a7171eb44574b0e8104c77b5b
John Klingler <jklingler@protonmail.com>
2019-03-04 22:31:27 -0500

    adds more work pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-function runOnScroll() {
-	let scrollPos = (document.documentElement.scrollTop > 0) ? document.documentElement.scrollTop : document.body.scrollTop;
-	document.body.className = "bg-gogrey";
-	if (scrollPos > 200) {
-    	document.body.className = "bg-myelomastars";
-	}
-	if (scrollPos > 800) {
-    	document.body.className = "bg-aizle";
-	}
-	if (scrollPos > 1500) {
-    	document.body.className = "bg-terrier";
-	}
-} 
-window.addEventListener("scroll", runOnScroll)
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
